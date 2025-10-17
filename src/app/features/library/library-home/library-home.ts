ab81673d1e764e1fdbb3bb423b942b7014b4f6bd
Alba Bernal <alba.bernal15@gmail.com>
2025-10-17 17:08:31 +0200

    Finalizando rebase y resolviendo conflicto en README.md

---
+++ src/app/features/library/library-home/library-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-library-home',
+  imports: [],
+  templateUrl: './library-home.html',
+  styleUrl: './library-home.css'
+})
+export class LibraryHome {
+
+}

3d3aa5063934610b02fb006418f9f6dfb6e9d820
AlbaBer15 <alba.bernal15@gmail.com>
2025-10-29 01:34:17 +0100

    crea pipe 'duration' con m y s

--- src/app/features/playlists/playlists-home/playlists-home.ts
+++ src/app/features/playlists/playlists-home/playlists-home.ts
@@ -1,32 +1,29 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // para [(ngModel)] en plantilla
+import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf, NgClass } from '@angular/common';
-import { DecimalPipe } from '@angular/common';
+import { DurationPipe } from '../../../shared/pipes/duration-pipe';
 
 @Component({
   selector: 'app-playlists-home',
   standalone: true,
-  imports: [FormsModule, NgFor, NgIf, NgClass, DecimalPipe],
+  imports: [FormsModule, NgFor, NgIf, NgClass, DurationPipe],
   templateUrl: './playlists-home.html',
   styleUrl: './playlists-home.css'
 })
 export class PlaylistsHome {
   title = 'Playlists';
   playlistName = 'Mi playlist';
-  coverUrl = '\musical-notes.png'; // opcional, para property binding a <img>
+  coverUrl = '\\musical-notes.png';
 
-  // Two-way binding y eventos
-  newTrack = '';       // campo de entrada para añadir canciones
-  filter = '';         // filtro de búsqueda
+  newTrack = '';
+  filter = '';
 
-  // Lista de canciones (base)
   tracks = [
     { title: 'City Lights', artist: 'Synth Academy', durationSec: 215, liked: false },
     { title: 'Analog Dreams', artist: 'Synth Academy', durationSec: 198, liked: true },
-    { title: 'Rainy Window', artist: 'Lo-Fi Lab',    durationSec: 241, liked: false }
+    { title: 'Rainy Window', artist: 'Lo-Fi Lab', durationSec: 241, liked: false }
   ];
 
-  // --- Métodos (event handling) ---
   addTrack() {
     const t = this.newTrack.trim();
     if (!t) return;
@@ -45,11 +42,8 @@ export class PlaylistsHome {
   removeTrack(index: number) {
     this.tracks.splice(index, 1);
   }
+
   trackByFn(index: number, track: any): number {
-  return track.id;
+    return index;
+  }
 }
-
-
-  // Filtro simple en plantilla (podrías mover esta lógica a un getter si prefieres)
-}
-
